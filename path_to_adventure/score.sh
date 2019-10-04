a3faf71f79d1999f3cf09a5a56d741d93ad2576f
Jason Baldus <jason.baldus@gmail.com>
2019-10-04 14:53:30 -0400

    Changes leave_home to outside
    Also updates scoring script to check for lower case names and
    to see if the student moved the bear to their treasure chest.

--- path_to_adventure/score.sh
+++ path_to_adventure/score.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 TOTAL=0
 
 function score_cave() {
-    if [ -f ./cottage/treasure_chest/treasure_cave ]; then
-        if [ ! -d ./cottage/leave_home/forest/cave ]; then
-            echo "CAVE Points: 2"
-            CAVE_POINTS=2
-            TOTAL=$((TOTAL+CAVE_POINTS))
-        else
-            echo "CAVE: Unfinished"
-        fi
+    if [ -f $DIR/cottage/treasure_chest/treasure_cave ] &&
+       [ ! -d $DIR/cottage/outside/forest/cave ]; then
+        CAVE_POINTS=2
+        echo "CAVE Points: 2"
+        TOTAL=$((TOTAL+CAVE_POINTS))
     else
         echo "CAVE: Unfinished"
     fi
 }
 
 function score_castle() {
-    LIBRARY=./cottage/leave_home/castle/library
-    if [ -r $LIBRARY ] && [ -w $LIBRARY ] && [ -x $LIBRARY ]; then
-        echo "CASTLE Points: 3"
+    LIBRARY=$DIR/cottage/outside/castle/library
+    if [ -r $LIBRARY ] && 
+       [ -w $LIBRARY ] && 
+       [ -x $LIBRARY ]; then
         CASTLE_POINTS=3
+        echo "CASTLE Points: 3"
         TOTAL=$((TOTAL+CASTLE_POINTS))
     else
         echo "CASTLE: Unfinished"
@@ -28,8 +29,7 @@ function score_castle() {
 }
 
 function score_desert() {
-    DESERT=./cottage/leave_home/desert
-    CORRECT="wateringhole_5 wateringhole_9 wateringhole_0 wateringhole_6 wateringhole_8 wateringhole_7 wateringhole_3 wateringhole_4 wateringhole_1 wateringhole_2"
+    DESERT=$DIR/cottage/outside/desert
     pushd $DESERT >/dev/null
     if [ wateringhole_5 -nt wateringhole_2 ] &&
        [ wateringhole_2 -nt wateringhole_1 ] &&
@@ -40,8 +40,8 @@ function score_desert() {
        [ wateringhole_8 -nt wateringhole_6 ] &&
        [ wateringhole_6 -nt wateringhole_0 ] &&
        [ wateringhole_0 -nt wateringhole_9 ]; then
-        echo "DESERT Points: 4"
         DESERT_POINTS=4
+        echo "DESERT Points: 4"
         TOTAL=$((TOTAL+DESERT_POINTS))
     else
         echo "DESERT: Unfinished"
@@ -50,9 +50,10 @@ function score_desert() {
 }
 
 function score_ruins() {
-    if [ -e ./cottage/treasure_chest/.treasure_ruins ] && [ ! $(ls ./cottage/leave_home/desert/ruins/*skeleton* 2>/dev/null) ]; then
-        echo "RUINS Points: 3"
+    if [ -e $DIR/cottage/treasure_chest/.treasure_ruins ] && 
+       [ ! $(ls $DIR/cottage/outside/desert/ruins/*skeleton* 2>/dev/null) ]; then
         RUINS_POINTS=3
+        echo "RUINS Points: 3"
         TOTAL=$((TOTAL+RUINS_POINTS))
     else
         echo "RUINS: Unfinished"
@@ -60,17 +61,18 @@ function score_ruins() {
 }
 
 function score_forest() {
-    if [ -e ./cottage/leave_home/forest/odin ]; then
-        echo "FOREST BONUS! 4 Points"
+    if [ -e $DIR/cottage/outside/forest/odin ]; then
         FOREST_POINTS=4
+        echo "FOREST BONUS! 4 Points"
         TOTAL=$((TOTAL+FOREST_POINTS))
     fi
 }
 
 function score_glen() {
-    if [ -e ./cottage/leave_home/forest/.hidden_glen/Elkimer ] && [ -e ./cottage/treasure_chest/.treasure_glen ]; then
-        echo "HIDDEN GLEN Points: 8"
+    if [ -e $DIR/cottage/outside/forest/.hidden_glen/elkimer ] && 
+       [ -e $DIR/cottage/treasure_chest/.treasure_glen ]; then
         GLEN_POINTS=8
+        echo "HIDDEN GLEN Points: 8"
         TOTAL=$((TOTAL+GLEN_POINTS))
     else
         echo "....Hmmm....Did you search everywhere?"
@@ -78,9 +80,9 @@ function score_glen() {
 }
 
 function score_library() {
-    if [ -e ./cottage/treasure_chest/treasure_373 ]; then
-        echo "LIBRARY Points: 6"
+    if [ -e $DIR/cottage/treasure_chest/treasure_373 ]; then
         LIBRARY_POINTS=6
+        echo "LIBRARY Points: 6"
         TOTAL=$((TOTAL+LIBRARY_POINTS))
     else
         echo "LIBRARY: Unfinished"
@@ -89,11 +91,11 @@ function score_library() {
 
 function score_cottage() {
     COTTAGE_SCORE=0
-    if [ -d ./cottage/treasure_chest ]; then
+    if [ -d $DIR/cottage/treasure_chest ]; then
+        COTTAGE_SCORE=$((COTTAGE_SCORE+1))
         echo "Treasure Chest Created: 1 Point"
-        COTTAGE_SCORE=1
     fi
-    if [ -e ./cottage/name.txt ]; then
+    if [ -e $DIR/cottage/name.txt ]; then
         echo "Name file created: 1 Point"
         COTTAGE_SCORE=$((COTTAGE_SCORE+1))
     fi
@@ -102,12 +104,22 @@ function score_cottage() {
     fi
     TOTAL=$((TOTAL+COTTAGE_SCORE))
 }
+
+function bear_in_treasure_chest() {
+    if [ -e $DIR/cottage/treasure_chest/bear ]; then
+        echo "You've put a bear in your treasure chest!"
+        echo "It eats all your points: -$TOTAL Points!"
+        TOTAL=0
+    fi
+}
+
 echo "This program will tell you how you have done."
 echo "============================================="
 echo "How did you do for the first steps?"
 score_cottage
 echo ""
 echo "Now, how about the rest?"
+echo "========================"
 score_forest
 score_castle
 score_cave
@@ -115,4 +127,6 @@ score_desert
 score_ruins
 score_library
 score_glen
+bear_in_treasure_chest
+echo "==================="
 echo "TOTAL SCORE: $TOTAL"
