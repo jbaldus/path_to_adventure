faaeef61a5a3618551f998124ff7d90a3edd5ac5
Jason Baldus <jason.baldus@gmail.com>
2025-02-08 19:32:04 -0500

    Start of modularization process

---
+++ path_to_adventure/.utils.sh
@@ -0,0 +1,15 @@
+# Some utility functions
+function reverse-filepath {
+    if [[ $# -ne 1 ]]; then
+        return
+    fi
+    local FILEPATH="$1"
+    local -a CHUNKS
+    while [[ "$FILEPATH" != "/" && "$FILEPATH" != "." ]]; do
+        CHUNKS+=("$(basename $FILEPATH)")
+        FILEPATH="$(dirname $FILEPATH)"
+    done
+    local REVERSED="${CHUNKS[*]}"
+    echo ${REVERSED// /-}
+
+}
---
+++ path_to_adventure/world/forest/__pta_resources/score.sh
@@ -0,0 +1,57 @@
+# This script will be sourced in the scoring script after the following
+# variables have been set up:
+# 
+# ITEMS: an array holding the score for individual locations
+# * This script should add new items for the things it cares about
+# POSSIBLE_POINTS: an array holding the possible points for each location
+# * Use the same key as for the ITEMS array
+# SCORING_FUNCTIONS: an array holding functions to be run each time the score 
+#   is determined in order to give points. 
+# * This script should define its own scoring function(s) and add it(them) 
+#   to the SCORING_FUNCTIONS array
+# DIR: points to the directory that holds the `world` directory
+# * DO NOT CHANGE THIS IN YOUR SCRIPT, OR IT WILL BREAK THE GAME!!!!
+# * $DIR/score is the path to the main scoring script
+# * $DIR/resources/<This module name> contains all the files in this 
+#   location's __pta_resources directory when the game is running. If you need
+#   to reference anything in that directory, this is how. It is also where this 
+#   script will be sourced from, for whatever that's worth.
+# WORLD: points to the directory that contains all the game's locations. 
+# * Use it to find paths that the player can access. For example:
+#   $WORLD/cottage/treasure_chest
+# CHEST: points to the treasure chest directory the player creates before 
+#        they can leave their cottage.
+# * Use it to check to see if they have collected any treasure you might be 
+#   interested in
+# =============================================================================
+# 
+# FOREST SCORING SCRIPTS
+# 
+# =============================================================================
+
+# ITEMS
+ITEMS[forest]=0
+ITEMS[glen]=0
+
+# POSSIBLE_POINTS
+POSSIBLE_POINTS[forest]=4
+POSSIBLE_POINTS[glen]=8
+
+# SCORING_FUNCTIONS
+function score_forest {
+    local FOREST=$WORLD/forest
+    if [ -e "$FOREST/odin" ]; then
+        ITEMS[forest]=4
+    fi
+}
+
+function score_glen {
+    local FOREST=$WORLD/forest
+    local $GLEN=$FOREST/.hidden_glen
+    if [ -e "$GLEN/elkimer" ] && 
+       [ -e "$CHEST/.treasure_glen" ]; then
+        ITEMS[glen]=8
+    fi
+}
+
+SCORING_FUNCTIONS+=(score_forest score_glen)
