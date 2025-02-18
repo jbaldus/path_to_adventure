96fd821726f8826356b52e8a8a0d514206c5832c
Jason Baldus <jason.baldus@gmail.com>
2025-02-17 19:16:07 -0500

    Use emphasis instead of bold for commands

--- path_to_adventure/.lib/welcome.sh
+++ path_to_adventure/.lib/welcome.sh
@@ -34,27 +34,27 @@ It's dangerous to go alone, so let me give you some important advice:
 
 > Nearly every place you go will have a README file. You should read it.
 
-> Remember that ${BOLD}man${RESET} is a command that will show you how other commands 
-  work. Use it like ${BOLD}man mv${RESET} to see what the ${BOLD}mv${RESET} command can do.
+> Remember that ${EMPHASIS}man${RESET} is a command that will show you how other commands 
+  work. Use it like ${EMPHASIS}man mv${RESET} to see what the ${BOLD}mv${RESET} command can do.
 
 EOF
 if [ $HARDMODE -eq 0 ]; then
 cat << EOF
-> If you really get stuck, you can type ${BOLD}help${RESET} to get a list of only the 
-  commands that you will need for this adventure. Use ${BOLD}man${RESET} for more info
+> If you really get stuck, you can type ${EMPHASIS}help${RESET} to get a list of only the 
+  commands that you will need for this adventure. Use ${BOEMPHASISD}man${RESET} for more info
   on each specific command.
 
 EOF
 fi
 
 cat << EOF
-> At any point you can type ${BOLD}score${RESET} to get a rundown of your score. If you
+> At any point you can type ${EMPHASIS}score${RESET} to get a rundown of your score. If you
   get 32 points, you win!
 
 Like all the best role-playing games, you will begin your journey in a
 humble cottage. Let's take you there now:
 
-Type ${BOLD}cd cottage${RESET} and then press ${BOLD}[Enter]${RESET}
+Type ${EMPHASIS}cd cottage${RESET} and then press ${EMPHASIS}[Enter]${RESET}
 
 EOF
 }
