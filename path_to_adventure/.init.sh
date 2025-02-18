400728019d3ee21600fd9f9149e43346085dc7f8
Jason E Baldus <jason.baldus@gmail.com>
2025-02-17 19:00:08 -0500

    Adds a score to the user prompt (#16)
    * Adds a score to the user prompt
    
    * Score in Hardmode prompt, too

--- path_to_adventure/.init.sh
+++ path_to_adventure/.init.sh
@@ -11,12 +11,19 @@ export WORLD=/World
 export PATH=$DIR:$PATH
 
 export BOLD=$'\e[1;31m'
+export EMPHASIS=$'\e[3;1;33m'
+export INVERT=$'\e[7m'
 export RESET=$'\e[0m'
 
 export HARDMODE=0
 
-export NORMALMODE_PROMPT='\[\e[01;32m\]\u@\h\[\e[0m\]:\[\e[01;34m\]\w\[\e[0m\] \[\e[01;33m\]\$\[\e[0m\] '
-export HARDMODE_PROMPT='${RESET}[P2A] \[\e[01;33m\]\$\[\e[0m\] '
+# PROMPT_SCORE moves the cursor all the way to the right, and then back 8 
+#  spaces and prints "Score: <score>". Then the "\n" prints a newline 
+# If we want the score to be on the right side of the SAME line as the 
+#  player types their commands, replace the "\n" with "\[\e[600D\]".
+export PROMPT_SCORE='\[\e[600C\e[8D\]$(__pta_style-score)\n'
+export NORMALMODE_PROMPT=$PROMPT_SCORE'\[\e[01;32m\]\u\[\e[0m\]:\[\e[01;34m\]\w\[\e[0m\] \[\e[01;33m\]\$\[\e[0m\] '
+export HARDMODE_PROMPT=$PROMPT_SCORE'${RESET}[P2A] \[\e[1;33m\]\$\[\e[0m\] '
 
 export PS1=$NORMALMODE_PROMPT
 
--- path_to_adventure/.lib/utils.sh
+++ path_to_adventure/.lib/utils.sh
@@ -39,3 +39,36 @@ function __pta_load_all { #directory
     done
 
 }
+
+
+# This function checks the score now and compares it to a previous score
+# It returns 0 if the score went up, and 1 if it didn't
+__pta_score-up() {
+    local prev_points
+    local now_points
+    if [[ ! -e $DIR/resources/prev_points ]]; then
+        echo 0 > $DIR/resources/prev_points
+        prev_points=0
+    else
+        prev_points=$(cat $DIR/resources/prev_points)
+    fi
+    now_points=$(score -q)
+    if [[ $now_points -gt $prev_points ]] ; then
+        echo $now_points > $DIR/resources/prev_points
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+#This will print the score with a red background if it went up 
+# on the last command
+__pta_style-score() {
+    if __pta_score-up; then
+        printf "${BOLD}${INVERT}Score: %2d\e[m" $(score -q)
+    else
+        printf "${RESET}Score: %2d" $(score -q)
+    fi
+
+}
